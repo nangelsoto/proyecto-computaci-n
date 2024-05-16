1aa5729fa523f66beeb596fd30edab4ec738487e
nangelsoto <122932482+nangelsoto@users.noreply.github.com>
2024-05-16 11:27:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ if uploaded_file is not None:
    st.write(df1)
    st.subheader('Estadísticos básicos de los sensores.')
    #convertir esto en variable
-   st.dataframe(df1["temperatura ESP32"].describe())
+   st.dataframe(df1["temperatura ESP32" or "humedad ESP32"].describe())
    
    min_temp = st.slider('Selecciona valor mínimo del filtro ', min_value=-10, max_value=45, value=23, key=1)
    # Filtrar el DataFrame utilizando query
