2c7393faa35c8843dec5527943fb9e01e1f840be
nangelsoto <122932482+nangelsoto@users.noreply.github.com>
2024-05-21 13:49:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,61 +6,43 @@ st.title('Control de Temperatura y Humedad Huerta Urbana')
 image = Image.open('grafana2.jpg')
 st.image(image)
 
-st.header("Cargar archivos CSV")
-uploaded_file_temp = st.file_uploader('Elige un archivo de temperatura', type=['csv'])
-uploaded_file_hum = st.file_uploader('Elige un archivo de humedad', type=['csv'])
+uploaded_file = st.file_uploader('Elige un archivo')
 
-def process_file(file, column_name):
-    df = pd.read_csv(file)
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+
+    st.subheader('Perfil gráfico de la variable medida.')
     df = df.set_index('Time')
-    return df[[column_name]]
+    st.line_chart(df)
 
-# Procesar archivo de temperatura
-if uploaded_file_temp is not None:
-    df_temp = process_file(uploaded_file_temp, "temperatura ESP32")
+    st.write(df)
+    st.subheader('Estadísticos básicos de los sensores.')
 
-    st.subheader('Perfil gráfico de la temperatura.')
-    st.line_chart(df_temp)
-    
-    st.write(df_temp)
-    st.subheader('Estadísticos básicos del sensor de temperatura.')
-    st.dataframe(df_temp["temperatura ESP32"].describe())
-    
-    min_temp = st.slider('Selecciona valor mínimo del filtro de temperatura', min_value=-10, max_value=45, value=23, key='min_temp')
-    filtrado_temp_min = df_temp.query(f"`temperatura ESP32` > {min_temp}")
+    # Filtrar por temperatura
+    min_temp = st.slider('Selecciona el valor mínimo del filtro de temperatura', min_value=-10, max_value=45, value=23, key=1)
+    filtrado_temp_min = df.query(f"`temperatura ESP32` > {min_temp}")
     st.subheader("Temperaturas superiores al valor configurado.")
-    st.write('Dataframe Filtrado')
+    st.write('Dataframe Filtrado (Temperatura)')
     st.write(filtrado_temp_min)
-    
-    max_temp = st.slider('Selecciona valor máximo del filtro de temperatura', min_value=-10, max_value=45, value=23, key='max_temp')
-    filtrado_temp_max = df_temp.query(f"`temperatura ESP32` < {max_temp}")
+
+    max_temp = st.slider('Selecciona el valor máximo del filtro de temperatura', min_value=-10, max_value=45, value=23, key=2)
+    filtrado_temp_max = df.query(f"`temperatura ESP32` < {max_temp}")
     st.subheader("Temperaturas inferiores al valor configurado.")
-    st.write('Dataframe Filtrado')
+    st.write('Dataframe Filtrado (Temperatura)')
     st.write(filtrado_temp_max)
-else:
-    st.warning('Necesitas cargar un archivo csv de temperatura.')
 
-# Procesar archivo de humedad
-if uploaded_file_hum is not None:
-    df_hum = process_file(uploaded_file_hum, "humedad ESP32")
-
-    st.subheader('Perfil gráfico de la humedad.')
-    st.line_chart(df_hum)
-    
-    st.write(df_hum)
-    st.subheader('Estadísticos básicos del sensor de humedad.')
-    st.dataframe(df_hum["humedad ESP32"].describe())
-    
-    min_hum = st.slider('Selecciona valor mínimo del filtro de humedad', min_value=0, max_value=100, value=50, key='min_hum')
-    filtrado_hum_min = df_hum.query(f"`humedad ESP32` > {min_hum}")
+    # Filtrar por humedad
+    min_hum = st.slider('Selecciona el valor mínimo del filtro de humedad', min_value=0, max_value=100, value=50, key=3)
+    filtrado_hum_min = df.query(f"`humedad ESP32` > {min_hum}")
     st.subheader("Humedades superiores al valor configurado.")
-    st.write('Dataframe Filtrado')
+    st.write('Dataframe Filtrado (Humedad)')
     st.write(filtrado_hum_min)
-    
-    max_hum = st.slider('Selecciona valor máximo del filtro de humedad', min_value=0, max_value=100, value=50, key='max_hum')
-    filtrado_hum_max = df_hum.query(f"`humedad ESP32` < {max_hum}")
+
+    max_hum = st.slider('Selecciona el valor máximo del filtro de humedad', min_value=0, max_value=100, value=50, key=4)
+    filtrado_hum_max = df.query(f"`humedad ESP32` < {max_hum}")
     st.subheader("Humedades inferiores al valor configurado.")
-    st.write('Dataframe Filtrado')
+    st.write('Dataframe Filtrado (Humedad)')
     st.write(filtrado_hum_max)
+
 else:
-    st.warning('Necesitas cargar un archivo csv de humedad.')
+    st.warning('Necesitas cargar un archivo CSV de Excel.')
