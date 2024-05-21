21747d965096158b027cc9355a3c14bb6d68da6a
nangelsoto <122932482+nangelsoto@users.noreply.github.com>
2024-05-21 12:50:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,38 +6,35 @@ st.title('Control de Temperatura y Humedad Huerta Urbana')
 image = Image.open('grafana2.jpg')
 st.image(image)
 
-uploaded_file = st.file_uploader('Choose a file')
+uploaded_file_temp = st.file_uploader('Elige un archivo de temperatura', key='temp')
+uploaded_file_hum = st.file_uploader('Elige un archivo de humedad', key='hum')
 
-if uploaded_file is not None:
-    # Leer el archivo CSV
-    df1 = pd.read_csv(uploaded_file)
+def process_file(uploaded_file, variable):
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        st.subheader(f'Perfil gráfico de la variable medida: {variable}.')
+        df = df.set_index('Time')
+        st.line_chart(df)
+        st.write(df)
+        st.subheader(f'Estadísticos básicos del sensor: {variable}.')
+        st.dataframe(df[variable].describe())
 
-    # Crear un selector para que el usuario elija entre 'temperatura ESP32' y 'humedad ESP32'
-    columna = st.selectbox(
-        'Seleccione la variable a visualizar',
-        ('temperatura ESP32', 'Time,"humedad ESP32"')
-    )
+        min_val = st.slider(f'Selecciona valor mínimo del filtro para {variable}', min_value=-10, max_value=100, value=23, key=f'min_{variable}')
+        filtrado_df_min = df.query(f"`{variable}` > {min_val}")
+        st.subheader(f"{variable.capitalize()} superiores al valor configurado.")
+        st.write('Dataframe Filtrado')
+        st.write(filtrado_df_min)
 
-    st.subheader('Perfil gráfico de la variable medida.')
-    df1 = df1.set_index('Time')
-    st.line_chart(df1[columna])
-    
-    st.write(df1)
-    st.subheader('Estadísticos básicos de los sensores.')
-    st.dataframe(df1[columna].describe())
-    
-    # Filtros de valores mínimos y máximos basados en la columna seleccionada
-    min_val = st.slider(f'Selecciona valor mínimo del filtro ({columna})', min_value=-10, max_value=100, value=23, key=1)
-    filtrado_df_min = df1.query(f"`{columna}` > {min_val}")
-    st.subheader(f"{columna.capitalize()} superiores al valor configurado.")
-    st.write('Dataframe Filtrado')
-    st.write(filtrado_df_min)
-    
-    max_val = st.slider(f'Selecciona valor máximo del filtro ({columna})', min_value=-10, max_value=100, value=23, key=2)
-    filtrado_df_max = df1.query(f"`{columna}` < {max_val}")
-    st.subheader(f"{columna.capitalize()} inferiores al valor configurado.")
-    st.write('Dataframe Filtrado')
-    st.write(filtrado_df_max)
+        max_val = st.slider(f'Selecciona valor máximo del filtro para {variable}', min_value=-10, max_value=100, value=23, key=f'max_{variable}')
+        filtrado_df_max = df.query(f"`{variable}` < {max_val}")
+        st.subheader(f"{variable.capitalize()} inferiores al valor configurado.")
+        st.write('Dataframe Filtrado')
+        st.write(filtrado_df_max)
+    else:
+        st.warning(f'Necesitas cargar un archivo de {variable}.')
 
-else:
-    st.warning('Necesitas cargar un archivo csv excel.')
+if uploaded_file_temp:
+    process_file(uploaded_file_temp, 'temperatura ESP32')
+
+if uploaded_file_hum:
+    process_file(uploaded_file_hum, 'humedad ESP32')
