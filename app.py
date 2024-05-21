a093fea3ade4c1fa5b6417e1b0422976769ce2ec
nangelsoto <122932482+nangelsoto@users.noreply.github.com>
2024-05-21 12:41:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,39 +9,35 @@ st.image(image)
 uploaded_file = st.file_uploader('Choose a file')
 
 if uploaded_file is not None:
+    # Leer el archivo CSV
     df1 = pd.read_csv(uploaded_file)
 
-    # Selección del nombre de la columna
-    columna = st.selectbox('Selecciona la columna que deseas visualizar:', options=['temperatura ESP32', 'humedad ESP32'])
+    # Crear un selector para que el usuario elija entre 'temperatura ESP32' y 'humedad ESP32'
+    columna = st.selectbox(
+        'Seleccione la variable a visualizar',
+        ('temperatura ESP32', 'humedad ESP32')
+    )
 
-    if 'Time' in df1.columns:
-        df1 = df1.set_index('Time')
-
-    if columna in df1.columns:
-        st.subheader('Perfil gráfico de la variable medida.')
-        st.line_chart(df1[columna])
-        
-        st.write(df1)
-        st.subheader('Estadísticos básicos de los sensores.')
-        st.dataframe(df1[columna].describe())
-        
-        min_value = st.slider(f'Selecciona valor mínimo del filtro ({columna})', min_value=-10.0, max_value=100.0, value=23.0, key=1)
-        # Filtrar el DataFrame utilizando query
-        filtrado_df_min = df1.query(f"`{columna}` > {min_value}")
-        # Mostrar el DataFrame filtrado
-        st.subheader(f"{columna} superiores al valor configurado.")
-        st.write('Dataframe Filtrado')
-        st.write(filtrado_df_min)
-        
-        max_value = st.slider(f'Selecciona valor máximo del filtro ({columna})', min_value=-10.0, max_value=100.0, value=23.0, key=2)
-        # Filtrar el DataFrame utilizando query
-        filtrado_df_max = df1.query(f"`{columna}` < {max_value}")
-        # Mostrar el DataFrame filtrado
-        st.subheader(f"{columna} inferiores al valor configurado.")
-        st.write('Dataframe Filtrado')
-        st.write(filtrado_df_max)
-    else:
-        st.error(f"La columna seleccionada '{columna}' no se encuentra en el archivo cargado.")
+    st.subheader('Perfil gráfico de la variable medida.')
+    df1 = df1.set_index('Time')
+    st.line_chart(df1[columna])
+    
+    st.write(df1)
+    st.subheader('Estadísticos básicos de los sensores.')
+    st.dataframe(df1[columna].describe())
+    
+    # Filtros de valores mínimos y máximos basados en la columna seleccionada
+    min_val = st.slider(f'Selecciona valor mínimo del filtro ({columna})', min_value=-10, max_value=100, value=23, key=1)
+    filtrado_df_min = df1.query(f"`{columna}` > {min_val}")
+    st.subheader(f"{columna.capitalize()} superiores al valor configurado.")
+    st.write('Dataframe Filtrado')
+    st.write(filtrado_df_min)
+    
+    max_val = st.slider(f'Selecciona valor máximo del filtro ({columna})', min_value=-10, max_value=100, value=23, key=2)
+    filtrado_df_max = df1.query(f"`{columna}` < {max_val}")
+    st.subheader(f"{columna.capitalize()} inferiores al valor configurado.")
+    st.write('Dataframe Filtrado')
+    st.write(filtrado_df_max)
 
 else:
     st.warning('Necesitas cargar un archivo csv excel.')
