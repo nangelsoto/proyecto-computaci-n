dc2dd9228fed782cbde1c3f2ba48aa9445aee2e9
nangelsoto <122932482+nangelsoto@users.noreply.github.com>
2024-05-28 15:28:13 -0500

    Update humedad.py

--- pages/humedad.py
+++ pages/humedad.py
@@ -1,1 +1,62 @@
+import pandas as pd
+import streamlit as st
+from PIL import Image
 
+# Título de la aplicación
+st.title('Control Humedad y Temperatura Huerta Urbana')
+image = Image.open('grafana2.jpeg')
+st.image(image)
+
+# Cargador de archivos
+uploaded_file = st.file_uploader('Choose a file')
+
+if uploaded_file is not None:
+    df1 = pd.read_csv(uploaded_file)
+
+    # Subtítulo y gráfico de línea
+    st.subheader('Humedad y Tiempo.')
+    df1 = df1.set_index('Time')
+    st.line_chart(df1)
+
+    st.write(df1)
+    st.subheader('Estadísticos básicos de los sensores.')
+    st.dataframe(df1["humedad ESP32"].describe())
+
+    # Slider para el valor mínimo del filtro
+    min_hum = st.slider('Selecciona valor mínimo del filtro ', min_value=-10, max_value=45, value=23, key=1)
+    # Filtrar el DataFrame utilizando query
+    filtrado_df_min = df1.query(f"`humedad ESP32` > {min_hum}")
+    # Alerta si el valor máximo es 45
+    if min_temp == -10:
+        st.write('<p style="color:red; font-size: 20px;">Alerta: valor mínimo seleccionado!</p>', unsafe_allow_html=True)
+        # Ruta al archivo de audio
+        audio_file = open('alarma.mp3', 'rb')
+        audio_bytes = audio_file.read()
+        st.audio(audio_bytes, format='audio/mp3')
+    # Mostrar el DataFrame filtrado
+    st.subheader("Humedades superiores al valor configurado.")
+    st.write('Dataframe Filtrado')
+    st.write(filtrado_df_min)
+
+    # Slider para el valor máximo del filtro
+    max_hum = st.slider('Selecciona valor máximo del filtro ', min_value=-10, max_value=45, value=23, key=2)
+    # Filtrar el DataFrame utilizando query
+    filtrado_df_max = df1.query(f"`humedad ESP32` < {max_hum}")
+        # Alerta si el valor máximo es 45
+    if max_temp == 45:
+        st.write('<p style="color:red; font-size: 20px;">Alerta: valor máximo seleccionado!</p>', unsafe_allow_html=True)
+        # Ruta al archivo de audio
+        audio_file = open('alerta.mp3', 'rb')
+        audio_bytes = audio_file.read()
+        st.audio(audio_bytes, format='audio/mp3')
+    # Mostrar el DataFrame filtrado
+    st.subheader("Humedades Inferiores al valor configurado.")
+    st.write('Dataframe Filtrado')
+    st.write(filtrado_df_max)
+
+
+
+
+
+else:
+    st.warning('Necesitas cargar un archivo csv excel.')
